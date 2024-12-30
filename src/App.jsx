b2418d729944a2ca18d746f7fe39ed9ec573d8c8
Siddiq05 <mustafasiddiq313@gmail.com>
2024-12-30 17:08:25 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./Components/Header";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
-import Projects from "./Components/projects";
+import Projects from "./Components/Projects";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
