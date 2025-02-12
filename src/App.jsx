e4f390384b6918f709f315520530cbf46f18d773
Siddiq05 <mustafasiddiq313@gmail.com>
2025-02-12 16:57:51 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -25,3 +25,55 @@ function App() {
 }
 
 export default App;
+
+// import "./App.css";
+// import Header from "./Components/Header";
+// import Home from "./Components/Home";
+// import About from "./Components/About";
+// import Contact from "./Components/Contact";
+// import Projects from "./Components/Projects";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// function App() {
+//   const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: (
+//         <>
+//           <Header /> <Home />
+//         </>
+//       ),
+//     },
+//     {
+//       path: "/about",
+//       element: (
+//         <>
+//           <Header /> <About />
+//         </>
+//       ),
+//     },
+//     {
+//       path: "/contact",
+//       element: (
+//         <>
+//           <Header /> <Contact />
+//         </>
+//       ),
+//     },
+//     {
+//       path: "/projects",
+//       element: (
+//         <>
+//           <Header /> <Projects />
+//         </>
+//       ),
+//     },
+//   ]);
+//   return (
+//     <>
+//       <RouterProvider router={router} />
+//     </>
+//   );
+// }
+
+// export default App;
